32197fd649c06a8ec3e3c466ee451ae9f96c92b1
JordiWillyam <jordiwillyamadikusuma_2226240138@mhs.mdp.ac.id>
2024-10-25 18:30:26 +0700

    ADD: produkController

---
+++ app_server/controllers/controllerProduk.js
@@ -0,0 +1,55 @@
+const Produk = require("../models/produk");
+
+const index = (req, res, next) => {
+    Produk.find({}, { __v: 0 })
+      .then((produk) => {
+        const responseMessage = {
+            code: 200,
+            success: true,
+            message: "Success",
+            data: produk
+        };
+        res.status(200).json(responseMessage);
+      })
+      .catch((e) => {
+        const responseMessage = {
+            code: 400,
+            success: false,
+            message: "Failed"
+        };
+        res.status(400).json(responseMessage);
+      });
+};
+
+//untuk menghandle request insert mahasiswa
+const insert = (req, res, next) => {
+    const mhs = new Mahasiswa({
+      nama: req.body.nama,
+      deskripsi: req.body.deskripsi,
+      harga: req.body.harga,
+      stok: req.body.tanggal_lahir,
+      kategori_id: true
+    });
+  
+    mhs
+      .save() // menyimpan data ke tabel
+      .then((result) => {
+            const responseMessage = {
+                code: 200,
+                success: true,
+                message: "Success",
+                data: result
+            };
+            res.status(200).json(responseMessage);
+        })
+        .catch((e) => {
+            const responseMessage = {
+                code: 400,
+                success: false,
+                message: "Failed"
+            };
+            res.status(400).json(responseMessage);
+        });
+};
+
+module.exports= {index,insert}
